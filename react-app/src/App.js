197ee4ef6272ce70d866920c601364c74209892b
anishbhanwala <anish.bhanwala@questionpro.com>
2020-10-26 21:31:47 +0530

    Added userprofile page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import { UserPorfile } from "./profile/UserProfile";
 import { Todos } from "./todos/Todos";
 
 function App(props) {
   return (
     <div className="App">
       <Todos />
+      <UserPorfile />
     </div>
   );
 }
---
+++ react-app/src/profile/UserProfile.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import styles from "./UserProfile.module.css";
+
+export const UserPorfile = (props) => {
+  const [firstname, setFirstname] = useState("anish");
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <label>First name: </label>
+        <input
+          type="text"
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+        ></input>
+      </div>
+    </div>
+  );
+};
